2a3eb10751b0da4fd54e706c4d14f91a5b1c6c60
Tom Needham <06needhamt@gmail.com>
2017-06-21 22:11:47 +0100

    Setup Functioning Windows Gradle Project

---
+++ TestApp/src/main/cpp/source/main.cpp
@@ -0,0 +1,7 @@
+#include "math.hpp"
+#include <iostream>
+
+int main(int argc, char** argv) {
+	std::cout << add(1, 1) << std::endl;
+	return EXIT_SUCCESS;
+}
---
+++ src/main/cpp/source/math.cpp
@@ -0,0 +1,5 @@
+#include "math.hpp"
+
+int add(int a, int b) {
+	return a + b;
+}
