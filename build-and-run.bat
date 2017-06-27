f421b95bf1c5d12eeab510d0862f0546b0aea496
Tom Needham <06needhamt@gmail.com>
2017-06-27 16:12:08 +0100

    Pipe Program Output to 'output.log'

--- build-and-run.bat
+++ build-and-run.bat
@@ -2,4 +2,4 @@
 call gradlew clean -POS=windows
 call gradlew build -POS=windows
 rem call gdb r build\exe\testApp\TestApp.exe
-call build\exe\testApp\TestApp.exe
+call build\exe\testApp\TestApp.exe > output.log
--- build-and-run.sh
+++ build-and-run.sh
@@ -2,4 +2,4 @@
 sudo ./gradlew clean -POS=linux
 sudo ./gradlew build -POS=linux
 # gdb r build\exe\testApp\TestApp.exe
-build\exe\testApp\TestApp.exe
+build/exe/testApp/TestApp.exe | output.log
