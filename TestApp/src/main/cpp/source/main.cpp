24b169f89db6ee58d9f7cf6e0b70b99a48878744
Tom Needham <06needhamt@gmail.com>
2017-06-27 13:39:43 +0100

    Rework Main for the New Mining Functionality

--- TestApp/src/main/cpp/source/main.cpp
+++ TestApp/src/main/cpp/source/main.cpp
@@ -21,26 +21,39 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-#include "platform.hpp"
-#include <iostream> // std::cout
+#include "platform.hpp" // Platform Specific Stuff NOTE: Must Always be the first include in a file
+#include <iostream> // std::cout, std::cin
 #include <thread> // std::this_thread
-#include <chrono> // std::chrono::milliseconds
-#include "block.hpp"
+#include <random> // rand(), srand()
+#include <chrono> // high_resolution_clock, std::chrono::time_point_cast, std::chrono::nanoseconds
+#include "block.hpp" // Block Stuff
+
 
 
 int main(int argc, char** argv) {
-	int d1 = 1 << 1;
-	int d2 = 1 << 2;
 
-	Block<int> b1 = Block<int>(&d1);
-	b1.setHeight(1);
-	b1.lock();
+	//Uncomment when debugging in Visual Studio
+	// char ch;
+	// std::cin >> ch;
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	
-	Block<int> b2 = Block<int>(&d2);
-	b2.setHeight(2);
-	b2.lock();
+	Block<int>* blocks[1000];
+	int data[1000];
 
+	auto t =  std::chrono::high_resolution_clock::now();
+	auto generator = std::chrono::time_point_cast<std::chrono::nanoseconds>(t).time_since_epoch().count();
+
+	for(int i = 0; i < 1000 - 1; i++){
+		t = std::chrono::high_resolution_clock::now();
+		generator = std::chrono::time_point_cast<std::chrono::nanoseconds>(t).time_since_epoch().count();
+		srand(generator);
+		data[i] = 1 + rand() % 1048575;
+		Block<int>* b = new Block<int>(&data[i]);
+		b->setHeight(i + 1);
+		b->mine();
+		b->lock();
+		blocks[i] = b;
+		std::cout << "Hash: " << blocks[i]->getHash() << " Height: " << blocks[i]->getHeight() << " Data: " << *(blocks[i]->getData()) << std::endl;
+
+	}
 	return 0;
 }
