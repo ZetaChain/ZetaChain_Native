0279cc3d9f10de855d49f10827aaef91529cc3c2
Tom Needham <06needhamt@gmail.com>
2017-07-22 14:57:24 +0100

    Update Build Scripts

--- build-and-run.bat
+++ build-and-run.bat
@@ -1,5 +1,5 @@
 @echo off
 call gradlew clean -POS=windows
 call gradlew build -POS=windows
-rem call gdb r build\exe\testApp\TestApp.exe
 call build\exe\testApp\TestApp.exe > output.log
+rem call build\exe\testApp\TestApp.exe --nosha256 > outputnoSha.log
