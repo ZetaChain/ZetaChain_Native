8069d832a844b8e42d155d6d3cbb4fe23e9abe6c
Tom Needham <06needhamt@gmail.com>
2017-07-22 14:54:26 +0100

    Implement Hashing Wrapper Functions

---
+++ src/main/cpp/include/hashing.hpp
@@ -0,0 +1,47 @@
+#pragma once 
+
+/*
+MIT License
+
+Copyright (c) 2017 Blockchain-VCS
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "platform.hpp" // Platform Specific Stuff NOTE: Must Always be the first include in a file
+#include <string>
+#include <vector>
+
+extern "C" unsigned char* HashASM(unsigned char* data, long size);
+
+namespace BlockchainCpp::Hashing {
+	std::string hashString(std::string str);
+	std::string hashChar(char ch);
+	std::string hashUnsignedChar(unsigned char uch);
+	std::string hashShort(short s);
+	std::string hashUnsignedShort(unsigned short us);
+	std::string hashInt(int i);
+	std::string hashUnsignedInt(unsigned int ui);
+	std::string hashLong(long l);
+	std::string hashUnsignedLong(unsigned long ul);
+	std::string hashLongLong(long long ll);
+	std::string hashUnsignedLongLong(unsigned long long ull);
+	std::string hashBoolean(bool b);
+	std::string hashVector(std::vector<unsigned char> vec);
+}
---
+++ src/main/cpp/source/hashing.cpp
@@ -0,0 +1,129 @@
+/*
+MIT License
+
+Copyright (c) 2017 Blockchain-VCS
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "platform.hpp" // Platform Specific Stuff NOTE: Must Always be the first include in a file
+#include <string>
+#include "hashing.hpp"
+#include "sha256.hpp"
+
+extern "C" unsigned char* HashASM(unsigned char* data, long size);
+extern bool __nosha256;
+
+namespace BlockchainCpp::Hashing {
+	std::string hashString(std::string str){
+		if(__nosha256) {
+			char* cstr = const_cast<char*>(str.c_str());
+			unsigned char* ptr = HashASM(reinterpret_cast<unsigned char*>(cstr), str.size());
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(str);
+	}
+	std::string hashChar(char ch){
+		if(__nosha256) {
+			unsigned char* ptr = HashASM(reinterpret_cast<unsigned char*>(&ch), sizeof(char));
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(std::string(1, ch));
+	}
+	std::string hashUnsignedChar(unsigned char uch){
+		if(__nosha256) {
+			unsigned char* ptr = HashASM(&uch, sizeof(unsigned char));
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(std::string(1, uch));
+	}
+	std::string hashShort(short s){
+		if(__nosha256) {
+			unsigned char* ptr = HashASM(reinterpret_cast<unsigned char*>(&s), sizeof(short));
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(std::string(1, s));
+	}
+	std::string hashUnsignedShort(unsigned short us){
+		if(__nosha256) {
+			unsigned char* ptr = HashASM(reinterpret_cast<unsigned char*>(&us), sizeof(unsigned short));
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(std::string(1, us));
+	}
+	std::string hashInt(int i){
+		if(__nosha256) {
+			unsigned char* ptr = HashASM(reinterpret_cast<unsigned char*>(&i), sizeof(int));
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(std::string(1, i));
+	}
+	std::string hashUnsignedInt(unsigned int ui){
+		if(__nosha256) {
+			unsigned char* ptr = HashASM(reinterpret_cast<unsigned char*>(&ui), sizeof(unsigned int));
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(std::string(1, ui));
+	}
+	std::string hashLong(long l){
+		if(__nosha256) {
+			unsigned char* ptr = HashASM(reinterpret_cast<unsigned char*>(&l), sizeof(long));
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(std::string(1, l));
+	}
+	std::string hashUnsignedLong(unsigned long ul){
+		if(__nosha256) {
+			unsigned char* ptr = HashASM(reinterpret_cast<unsigned char*>(&ul), sizeof(unsigned long));
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(std::string(1, ul));
+	}
+	std::string hashLongLong(long long ll){
+		if(__nosha256) {
+			unsigned char* ptr = HashASM(reinterpret_cast<unsigned char*>(&ll), sizeof(long long));
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(std::string(1, ll));
+	}
+	std::string hashUnsignedLongLong(unsigned long long ull){
+		if(__nosha256) {
+			unsigned char* ptr = HashASM(reinterpret_cast<unsigned char*>(&ull), sizeof(unsigned long long));
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(std::string(1, ull));
+	}
+	std::string hashBoolean(bool b){
+		if(__nosha256) {
+			unsigned char* ptr = HashASM(reinterpret_cast<unsigned char*>(&b), sizeof(bool));
+			return std::string(ptr, ptr + 32);
+		}
+		return sha256(std::string(1, b));
+	}
+
+	std::string hashVector(std::vector<unsigned char> vec){
+		std::string str = "";
+		if(vec.size() == 0)
+			return "";
+		for(int i = 0; i < vec.size() - 1; i++){
+			str += vec[i];
+		}
+		return hashString(str);
+	}
+}
