a8109c8e0b1eb17c925c1d5c28200ade97f18ca6
Tom Needham <06needhamt@gmail.com>
2017-09-25 19:23:38 +0100

    (#12) Fixed Linux Build File Line Endings
    close #12

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
-rm -f TestApp
-sudo ./gradlew clean -POS=linux
-sudo ./gradlew build -POS=linux
-cp -f build/testApp/TestApp TestApp
-# gdb r build\testApp\TestApp
-build/testApp/TestApp | output.log
+#!/usr/bin/env bash 
+rm -f TestApp 
+sudo ./gradlew clean -POS=linux 
+sudo ./gradlew build -POS=linux 
+cp -f build/testApp/TestApp TestApp 
+# gdb r build\testApp\TestApp 
+build/testApp/TestApp | output.log 
