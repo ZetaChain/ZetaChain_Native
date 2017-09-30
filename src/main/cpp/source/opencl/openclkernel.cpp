2a3cc262e0023e85943de59f51ff1863fa585157
Tom Needham <06needhamt@gmail.com>
2017-09-30 14:11:11 +0100

    Implement OpenCL Kernel Abstraction

---
+++ src/main/cpp/source/opencl/openclkernel.cpp
@@ -0,0 +1,59 @@
+#pragma once
+/*
+MIT License
+
+Copyright (c) 2017 ZetaChain_Native
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "platform.hpp" // Platform Specific Stuff NOTE: Must Always be the first include in a file
+#include <vector>
+#include <string>
+#include "opencl/openclhandle.hpp"
+#include "opencl/openclkernel.hpp"
+#include "opencl/opencldata.hpp"
+
+namespace ZetaChain_Native::OpenCL {
+	OpenCLKernel::OpenCLKernel(cl_kernel kernel, std::string name, OpenCLHandle** handle) {
+		this->kernel = kernel;
+		this->name = name;
+		this->handle = handle;
+	}
+
+	OpenCLKernel::~OpenCLKernel() {
+		(*handle)->releaseProgram(this->program);
+	}
+
+	cl_kernel OpenCLKernel::getProgram() {
+		return this->program;
+	}
+
+	std::string OpenCLKernel::getName() {
+		return this->name;
+	}
+
+	OpenCLHandle* OpenCLKernel::getHandle() {
+		return *this->handle;
+	}
+
+	bool OpenCLKernel::isHandleValid() {
+		return OpenCLData::getInstance()->handle == (*this->handle);
+	}
+}
